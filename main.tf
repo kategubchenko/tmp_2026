f3405afe93f7cbf96569ff7f8fb43c8452e1727e
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-03 15:26:49 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "run_id" {
+  default = "run_id_2"
+  sensitive = true
+}
+
+variable "sleep_time" {
+  default = 19
+}
+
+resource "random_integer" "timeout" {
+  count = 5
+ 
+  min = 32
+  max = 160
+
+  keepers = {
+    run_id = var.run_id
+  }
+}
+
+
+
+resource "null_resource" "env_vars" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
