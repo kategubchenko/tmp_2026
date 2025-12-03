4a9eeefb34f57f1cbf5e42352ff37fae1febcca4
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-03 15:27:22 +0100

    Create main.tf

---
+++ string/main.tf
@@ -0,0 +1,14 @@
+resource "random_string" "test" {
+  count = var.string_count
+  length = var.string_length
+}
+
+variable "string_count" {
+  sensitive = false
+  default = "2"
+}
+
+variable "string_length" {
+  sensitive = false
+  default = "2"
+}
