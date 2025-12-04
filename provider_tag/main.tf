e9a17e2d13a5a7ce21f2da25de7222b778e390e6
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-04 17:16:37 +0100

    Create main.tf

---
+++ provider_tag/main.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "3.5.0"
+        }
+    }
+}
+
+
+resource "scalr_tag" "example-x" {
+  name       = "tag-x"
+}
