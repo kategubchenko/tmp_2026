ec3af34064e029bee8943cba3ba139d71ab4e15c
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-04 17:14:01 +0100

    Create main.tf

---
+++ provider_ws/main.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "3.5.0"
+        }
+    }
+}
+
+resource "scalr_workspace" "cli-driven" {
+  name            = "cli-from_provider"
+  environment_id  = data.scalr_environment.test.id
+  deletion_protection_enabled = "false"
+}
