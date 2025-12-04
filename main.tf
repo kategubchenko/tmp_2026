80e73e9ffbc3dfbfb6313618bde349e21625dab5
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-04 15:38:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,7 @@ terraform {
 resource "scalr_tag" "example" {
   name       = "tag-1"
 }
-resource "scalr_tag" "example2" {
-  name       = "tag-2"
-}
+
 
 variable "run_id" {
   default = "run_id_2"
