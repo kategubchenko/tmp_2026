b2e95a7a3ad3785e07edb1ddb18b6008ef4f36b2
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-04 10:24:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "3.5.0"
+        }
+    }
+}
+
+data "scalr_environment" test {
+  name = "Environment-A"
+}
+
+resource "scalr_workspace" "cli-driven" {
+  name            = "cli-from_provider"
+  environment_id  = data.scalr_environment.test.id
+  deletion_protection_enabled = "false"
+}
+
 variable "run_id" {
   default = "run_id_2"
   sensitive = true
