2561e05df49b09b98d31cc78292469d45adfce8d
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-19 12:50:41 +0000

    commit to ws3

--- priveder_ws/main.tf
+++ priveder_ws/main.tf
@@ -12,7 +12,7 @@ data "scalr_environment" test {
 }
 
 resource "scalr_workspace" "cli-driven" {
-  name            = "cli-from_provider-wtf"
+  name            = "cli-from_provider-ws5"
   environment_id  = data.scalr_environment.test.id
   deletion_protection_enabled = "false"
 }
