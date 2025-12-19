7fba1f72c468a2b7798e2ef2f3f1e9424a31b4b3
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-19 10:16:58 +0000

    Update main.tf

--- priveder_ws/main.tf
+++ priveder_ws/main.tf
@@ -12,7 +12,7 @@ data "scalr_environment" test {
 }
 
 resource "scalr_workspace" "cli-driven" {
-  name            = "cli-from_providerX"
+  name            = "cli-from_providerY"
   environment_id  = data.scalr_environment.test.id
   deletion_protection_enabled = "false"
 }
