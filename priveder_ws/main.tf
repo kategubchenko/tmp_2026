a190e5a7116f650ff640c06548b420e1e286b59d
Kateryna Gubchenko <porotunya@gmail.com>
2025-12-22 16:13:28 +0000

    bbbbb

--- priveder_ws/main.tf
+++ priveder_ws/main.tf
@@ -12,7 +12,7 @@ data "scalr_environment" test {
 }
 
 resource "scalr_workspace" "cli-driven" {
-  name            = "cli-from_provider-f"
+  name            = "cli-from_provider-ff"
   environment_id  = data.scalr_environment.test.id
   deletion_protection_enabled = "false"
 }
