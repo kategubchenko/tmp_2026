73227d6875affcc73d395231fb1e9580d043f370
Kateryna Gubchenko <porotunya@gmail.com>
2026-01-22 18:56:49 +0000

    Update main.tf

--- string/main.tf
+++ string/main.tf
@@ -5,10 +5,10 @@ resource "random_string" "test" {
 
 variable "string_count" {
   sensitive = false
-  default = "12"
+  default = "2"
 }
 
 variable "string_length" {
   sensitive = false
-  default = "2"
+  default = "28"
 }
