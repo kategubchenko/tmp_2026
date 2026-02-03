3267c54965849a0fc07341c29935d142f391d16d
Kateryna Gubchenko <katerynagubchenko@Katerynas-MacBook-Air-2.local>
2026-02-03 14:24:49 +0200

    update ws

--- priveder_ws/main.tf
+++ priveder_ws/main.tf
@@ -12,7 +12,7 @@ data "scalr_environment" test {
 }
 
 resource "scalr_workspace" "cli-driven" {
-  name            = "cli-from_provider-fix"
+  name            = "cli-from_provider-fix-sync"
   environment_id  = data.scalr_environment.test.id
   deletion_protection_enabled = "false"
 }
