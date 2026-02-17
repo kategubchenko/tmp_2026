c0e22e866b6467ed73948ab88623821109a56e34
Kateryna Gubchenko <porotunya@gmail.com>
2026-02-17 11:49:19 +0000

    Update main.tf

--- provider_pc/main.tf
+++ provider_pc/main.tf
@@ -8,23 +8,15 @@ terraform {
     }
 }
 
-(* 
-variable "pc_count" {
-  sensitive = false
-} *)
 
 resource "scalr_provider_configuration" "kubernetes" {
-  (* count = var.pc_count *)
   name       = "k8s"
-  # account_id = "acc-svrcncgh453bi8g"
   custom {
     provider_name = "kubernetes"
 
     argument {
       name        = "host"
       value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c"
-      (* value       = "https://kubernetes.io/flyscan1/hyperspectral/hydrocarbon-detection-quantification/cpp/argus:0.11.0-rc4@sha256:096e5910715111c60af9d804a455471b626c20ded014bb226c7947decd81d07c" *)
-
       description = "The hostname (in form of URI) of the Kubernetes API."
     }
   }
